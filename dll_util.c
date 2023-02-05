33c6d0d84eab4d715f070057c8f01be8298f9f0a
ajithpinninti <ajitpinninti@gmail.com>
2023-02-05 16:30:47 +0530

    need

---
+++ dll_util.c
@@ -0,0 +1,22 @@
+#include "doubly_ll.h"
+#include"stdio.h"
+#include "stdlib.h"
+
+
+/*return 0 on succes -1 on failure */
+
+int remove_data_from_dll(dll_t *dll, void *data){
+    
+    return 0;
+}
+
+/*return 0 if empty. -1 if no empty*/
+int is_dll_empty(dll_t *dll){
+    return 0;
+}
+
+/*delete all the nodes froma a dll, but do not free the applnn data*/
+void drain_dll(dll_t *dll){
+
+    return;
+}
--- doubly_ll.h
+++ doubly_ll.h
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-
 /*Header for Doubly Linked list*/
 
 typedef struct doubly_node
@@ -19,4 +18,16 @@ typedef struct dll_
 return a new empty doubly linked list*/
 
 dll_t *get_new_dll();
+
+/* Adding data to the created Doubly linked liist*/
 int add_data_to_dll(dll_t *dll, void *app_data);
+
+/*return 0 on succes -1 on failure */
+
+int remove_data_from_dll(dll_t *dll, void *data);
+
+/*return 0 if empty. -1 if no empty*/
+int is_dll_empty(dll_t *dll);
+
+/*delete all the nodes froma a dll, but do not free the applnn data*/
+void drain_dll(dll_t *dll);
